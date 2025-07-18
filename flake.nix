f7f6de3da37dd1997772856ff0056b82960e15f9
Pavel Popov <me@popov.wtf>
2025-07-19 00:40:16 +0800

    Add trusted public keys and substituters to `flake.nix` for `nixConfig`

--- flake.nix
+++ flake.nix
@@ -50,6 +50,11 @@
     };
   };
 
+  nixConfig = {
+    extra-trusted-public-keys = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
+    extra-substituters = "https://nix-community.cachix.org";
+  };
+
   outputs = {
     self,
     nixpkgs,
--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -23,12 +23,6 @@
         "@admin"
         "${userConfig.username}"
       ];
-      extra-substituters = [
-        "https://nix-community.cachix.org"
-      ];
-      extra-trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
     };
 
     gc = {
