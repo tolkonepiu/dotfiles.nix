7e4c9b7a3aa78ba6d1b8001fe4116e50f56e7120
Pavel Popov <me@popov.wtf>
2025-07-19 00:58:28 +0800

    chore: Refactor project configuration files

--- flake.nix
+++ flake.nix
@@ -105,7 +105,7 @@
           modules = [
             {
               enterTest = ''
-                nh darwin build --hostname "${system}" --no-nom . -- --accept-flake-config
+                nix run .#build -- --accept-flake-config
               '';
 
               git-hooks.hooks = {
