f60a9c6e071025f95df7d1fc15d665559a695713
Pavel Popov <me@popov.wtf>
2025-09-01 13:34:39 +0800

    feat(ssh): disable deprecated default config and configure explicit SSH settings

--- modules/home/ssh.nix
+++ modules/home/ssh.nix
@@ -6,20 +6,35 @@
 }: let
   inherit (pkgs.stdenv.hostPlatform) isDarwin;
 in {
-  # Create the Secretive SSH configuration (Only for macOS)
-  home.file.".ssh/config_secretive" = lib.mkIf isDarwin {
-    text = ''
-      Host *
-        IdentityAgent ${config.home.homeDirectory}/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh
-    '';
-  };
-
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
 
     includes = [
-      (lib.mkIf isDarwin "${config.home.homeDirectory}/.ssh/config_secretive")
       "${config.home.homeDirectory}/.ssh/config_external"
+    ];
+
+    matchBlocks = lib.mkMerge [
+      {
+        "*" = {
+          forwardAgent = false;
+          addKeysToAgent = "yes";
+          compression = true;
+          serverAliveInterval = 60;
+          serverAliveCountMax = 3;
+          hashKnownHosts = true;
+          userKnownHostsFile = "${config.home.homeDirectory}/.ssh/known_hosts";
+          controlMaster = "auto";
+          controlPath = "${config.home.homeDirectory}/.ssh/master-%r@%n:%p";
+          controlPersist = "10m";
+        };
+      }
+
+      (lib.mkIf isDarwin {
+        "*" = {
+          identityAgent = "${config.home.homeDirectory}/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh";
+        };
+      })
     ];
   };
 }
