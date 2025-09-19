7f07e614fc695271391d80c78096a959e2e7f96a
Pavel Popov <me@popov.wtf>
2025-09-19 16:11:02 +0800

    feat(ghostty): add mouse scroll multiplier and shell integration settings

--- modules/home/ghostty.nix
+++ modules/home/ghostty.nix
@@ -10,10 +10,13 @@ in {
     enableZshIntegration = true;
     package = lib.mkIf isDarwin pkgs.ghostty-bin;
     settings = {
+      confirm-close-surface = false;
       font-family = "MesloLGS NF";
       font-size = 18;
       fullscreen = true;
       mouse-hide-while-typing = true;
+      mouse-scroll-multiplier = 10;
+      shell-integration-features = "no-cursor,no-ssh-terminfo,ssh-env,sudo,title";
     };
   };
 }
