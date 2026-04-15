8782cb419bdb3df908bf34348fabfd81e3eeee61
Pavel Popov <me@popov.wtf>
2026-04-15 17:08:25 +0800

    chore(atuin): improve history filtering and search defaults

--- modules/home/atuin.nix
+++ modules/home/atuin.nix
@@ -5,6 +5,11 @@
     settings = {
       auto_sync = true;
       sync_address = config.me.atuinServer;
+      filter_mode = "host";
+      search_mode = "fuzzy";
+      history_filter = [
+        "^gopass($| )"
+      ];
     };
   };
 }
