1bcb800dd0859c6f4c6515e8018b3bafe685d0ac
Pavel Popov <me@popov.wtf>
2026-04-27 02:43:40 +0800

    feat(packages): switch Iosevka to Iosevka Term

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -54,7 +54,7 @@
       # Fonts
       nerd-fonts.departure-mono
       nerd-fonts.jetbrains-mono
-      nerd-fonts.iosevka
+      nerd-fonts.iosevka-term
     ]
     ++ lib.optionals stdenv.isDarwin [
       # Virtualization
