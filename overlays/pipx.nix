c8ba3afd30132378fc5cac5d0a80c67ea9d1aa83
Pavel Popov <me@popov.wtf>
2026-07-27 15:26:16 +0800

    fix(pipx): skip failing inject test file

--- overlays/pipx.nix
+++ overlays/pipx.nix
@@ -1,6 +1,12 @@
 # See https://github.com/NixOS/nixpkgs/issues/522307
 _: _final: prev: {
   pipx = prev.pipx.overridePythonAttrs (old: {
+    disabledTestPaths =
+      (old.disabledTestPaths or [])
+      ++ [
+        "tests/test_inject.py"
+      ];
+
     disabledTests =
       (old.disabledTests or [])
       ++ [
